d5e3a4e92d2b767f396e364530a7c5d37d279046
bearlen <zhoehberga@hotmail.com>
2022-03-05 13:35:08 +0800

    eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: "babel-eslint"
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    "no-mixed-spaces-and-tabs": ["warn"],
+    "curly": ["warn"],
+    "no-unused-vars": ["warn"],
+    "comma-dangle": 0,
+    "semi": "never"
+  }
+}
