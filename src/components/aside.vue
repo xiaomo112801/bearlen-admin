f4fb8c80529511f42f2a5482661e709e81b82260
bearlen <zhoehberga@hotmail.com>
2022-03-08 00:08:21 +0800

    aside动画

--- src/components/aside.vue
+++ src/components/aside.vue
@@ -33,7 +33,6 @@
             <el-menu-item index="1-1">item one</el-menu-item>
             <el-menu-item index="1-2">item one</el-menu-item>
             <el-menu-item index="1-3">item three</el-menu-item>
-
             <el-sub-menu index="1-4">
               <template #title>item four</template>
               <el-menu-item index="1-4-1">item one</el-menu-item>
