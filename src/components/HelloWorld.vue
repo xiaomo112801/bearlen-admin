bbcea5e5c0db9ee4b616a57e571c7c9d939512a0
bearlen <zhoehberga@hotmail.com>
2022-03-07 10:57:22 +0800

    代码格式纠正

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,44 +2,44 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
+      For a guide and recipes on how to configure / customize this project,<br/>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
+      >vue-cli documentation</a
       >.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li>
         <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
+            href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+            target="_blank"
+            rel="noopener"
+        >babel</a
         >
       </li>
       <li>
         <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
+            href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
+            target="_blank"
+            rel="noopener"
+        >router</a
         >
       </li>
       <li>
         <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
+            href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
+            target="_blank"
+            rel="noopener"
+        >vuex</a
         >
       </li>
       <li>
         <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
+            href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+            target="_blank"
+            rel="noopener"
+        >eslint</a
         >
       </li>
     </ul>
@@ -50,17 +50,17 @@
       </li>
       <li>
         <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
+        >Forum</a
         >
       </li>
       <li>
         <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
+        >Community Chat</a
         >
       </li>
       <li>
         <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
+        >Twitter</a
         >
       </li>
       <li>
@@ -71,7 +71,7 @@
     <ul>
       <li>
         <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
+        >vue-router</a
         >
       </li>
       <li>
@@ -79,23 +79,23 @@
       </li>
       <li>
         <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
+            href="https://github.com/vuejs/vue-devtools#vue-devtools"
+            target="_blank"
+            rel="noopener"
+        >vue-devtools</a
         >
       </li>
       <li>
         <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
+        >vue-loader</a
         >
       </li>
       <li>
         <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
+            href="https://github.com/vuejs/awesome-vue"
+            target="_blank"
+            rel="noopener"
+        >awesome-vue</a
         >
       </li>
     </ul>
@@ -107,8 +107,8 @@ export default {
   name: "HelloWorld",
   props: {
     msg: String,
-  },
-};
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -116,14 +116,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
