4568709489d8dc423976e37cc857d4a31b012ebb
bearlen <zhoehberga@hotmail.com>
2022-03-09 01:06:02 +0800

    aside列表渲染

--- src/components/asideIcon.vue
+++ src/components/asideIcon.vue
@@ -2,12 +2,10 @@
 import { h, resolveComponent } from "vue"
 
 export default {
-  template: "#aside-icon-template",
   props: {
     icon: String
   },
   render() {
-    console.log(this.$slots)
     const ELicon = resolveComponent(this.icon)
     return h(
       ELicon
