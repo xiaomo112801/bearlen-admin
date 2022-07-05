686f82cae95cde9f7ae1e8852ff55b837be92e65
bearlen <zhoehberga@hotmail.com>
2022-07-05 12:26:11 +0800

    echarts图标

--- src/views/dashboard/workplace.vue
+++ src/views/dashboard/workplace.vue
@@ -4,10 +4,25 @@
   </el-container>
 </template>
 
-<script>
-export default {
-  name: "workplace"
-}
+<script setup>
+
+//import * as echarts from 'echarts/core'
+//import { BarChart, PieChart } from 'echarts/charts'
+//import {
+//  TitleComponent, //标题
+//  TooltipComponent, //提示框
+//  GridComponent, //坐标系
+//  DatasetComponent, //数据集
+//  TransformComponent //内置数据转换
+//} from 'echarts/components'
+//// 标签自动布局，全局过渡动画等特性
+//import { LabelLayout, UniversalTransition } from 'echarts/features'
+//// 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
+//import { CanvasRenderer } from 'echarts/renderers'
+
+
+
+
 </script>
 
 <style scoped>
