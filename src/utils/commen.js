a52de758b31f1e41ffba7e60acfbfc9fe46e8ee0
bearlen <zhoehberga@hotmail.com>
2022-07-14 11:29:08 +0800

    数据验证方法封装

---
+++ src/utils/commen.js
@@ -0,0 +1,32 @@
+import { unref } from 'vue'
+import { api } from '@/utils/request'
+// import { ElMessage } from "element-plus"
+
+//element form表单验证后提交数据
+export const validateDataThenSubmit = (refData, url, submitData) => {
+  const unRefData = unref(refData)
+  if (!unRefData) {
+    return Promise.resolve(false)
+  }
+
+  return unRefData.validate(valid => {
+    if (valid) {
+      api.post(url, submitData).then(res => {
+        return Promise.resolve(res)
+      })
+    } else {
+      return Promise.resolve(false)
+    }
+  })
+}
+
+//element form 表单验证(仅验证数据)
+export const validateData = (refData) => {
+  const unRefData = unref(refData)
+  if (!unRefData) {
+    return false
+  }
+  return unRefData.validate(valid => {
+    return valid
+  })
+}
