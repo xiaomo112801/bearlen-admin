87dfde6d79fe85c18a9f58cbf288d8eb0980b610
bearlen <zhoehberga@hotmail.com>
2022-07-14 11:32:32 +0800

    接口地址修改

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/admin': {
-        target: "http://bearlen-admin.test/",
+        target: "http://api.bearlen.com",
         changeOrigin: true,
         ws: true
       }
